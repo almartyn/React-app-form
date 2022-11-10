3deb9e058410cec4e434196bc2ce42009e620bf7
Almartyn <7590970@gmail.com>
2022-11-10 21:09:08 +0200

    add portals

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,23 +1,47 @@
+import React from "react";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
+import ReactDom from "react-dom";
 
-const ErrorModal = (props) => {
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onClouseModal}></div>;
+};
+
+const Modal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onClouseModal}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h1>{props.title}</h1>
-        </header>
-        <div className={styles.content}>
-          <p>{props.massage}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onClouseModal}>Закрыть</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h1>{props.title}</h1>
+      </header>
+      <div className={styles.content}>
+        <p>{props.massage}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onClouseModal}>Закрыть</Button>
+      </footer>
+    </Card>
   );
 };
+
+const ErrorModal = (props) => {
+  const modalRootElement = document.querySelector("#modal");
+  return (
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onClouseModal={props.onClouseModal} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDom.createPortal(
+        <Modal
+          title={props.title}
+          massage={props.massage}
+          onClouseModal={props.onClouseModal}
+        />,
+        modalRootElement
+      )}
+    </React.Fragment>
+  );
+};
+
 export default ErrorModal;
